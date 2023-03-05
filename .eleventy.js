4efc832c2d4d15fa850ef90749b9e0ffbd538fa8
xhy <haoyang@expoundite.net>
2023-03-05 10:52:11 +0800

    create post filter

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
-	components: "_includes/components/**/*.webc",
-});
-};
+		components: "_includes/components/**/*.webc",
+	});
+	eleventyConfig.addCollection("post", function(collectionApi) {
+		return collectionApi.getFilteredByGlob("_posts/*.md");
+	});
+}
