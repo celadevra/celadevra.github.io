edda15aa278e33f2fcdf4632c47f4449c5eaee82
xhy <haoyang@expoundite.net>
2023-03-05 11:39:05 +0800

    use limax to convert url

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,17 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const slug = require("limax");
 
 module.exports = function(eleventyConfig) {
+	// define location for WebC components
 	eleventyConfig.addPlugin(pluginWebc, {
 		components: "_includes/components/**/*.webc",
 	});
+	// define post collection by input path
 	eleventyConfig.addCollection("post", function(collectionApi) {
 		return collectionApi.getFilteredByGlob("_posts/*.md");
+	});
+	// use limax to create slug from titles
+	eleventyConfig.addFilter("unislug", function(value) {
+		return slug(value);
 	});
 }
